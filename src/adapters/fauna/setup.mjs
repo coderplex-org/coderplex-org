bf3b2471fa8b65a12f4544b16640b0f60a17fa52
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-01-31 15:59:20 +0530

    Add LinkedIn provider

--- src/adapters/fauna/setup.mjs
+++ src/adapters/fauna/setup.mjs
@@ -1,7 +1,11 @@
 import faunadb from 'faunadb'
 const q = faunadb.query
+const isProduction = process.env.NODE_ENV === 'production'
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET,
+  scheme: isProduction ? 'https' : 'http',
+  domain: isProduction ? 'db.fauna.com' : 'localhost',
+  port: 8443,
 })
 
 async function main() {
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,15 @@
 import NextAuth, { InitOptions } from 'next-auth'
 import Providers from 'next-auth/providers'
 import { NextApiHandler } from 'next'
-import faunadb from 'faunadb'
 import Fauna from '@/adapters'
 
+import faunadb from 'faunadb'
+const isProduction = process.env.NODE_ENV === 'production'
 const faunaClient = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET,
+  scheme: isProduction ? 'https' : 'http',
+  domain: isProduction ? 'db.fauna.com' : 'localhost',
+  port: 8443,
 })
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
@@ -18,16 +22,9 @@ const options: InitOptions = {
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'user:email',
     }),
-    Providers.Email({
-      server: {
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASSWORD,
-        },
-      },
-      from: process.env.SMTP_FROM,
+    Providers.LinkedIn({
+      clientId: process.env.LINKEDIN_ID,
+      clientSecret: process.env.LINKEDIN_SECRET,
     }),
   ],
   adapter: Fauna.Adapter({ faunaClient }),
