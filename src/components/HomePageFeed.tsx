3cc06a471fb34d7f582ceea9cc2601c873bfc8c4
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-02-17 11:25:57 +0530

    Remove the filter in the home page

--- src/components/HomePageFeed.tsx
+++ src/components/HomePageFeed.tsx
@@ -233,65 +233,8 @@ export default function HomePageFeed({
 }) {
   return (
     <>
-      <div className="px-4 sm:px-0">
-        <div className="sm:hidden">
-          <label htmlFor="question-tabs" className="sr-only">
-            Select a tab
-          </label>
-          <select
-            id="question-tabs"
-            className="block w-full rounded-md border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-brand-500 focus:ring-brand-500"
-          >
-            <option value="#/recent">Recent</option>
-            <option value="#/most-liked">Most Liked</option>
-            <option value="#/most-answers">Most Answers</option>
-          </select>
-        </div>
-        <div className="hidden sm:block">
-          <nav
-            className="relative z-0 rounded-lg shadow flex divide-x divide-gray-200"
-            aria-label="Tabs"
-          >
-            <a
-              href="/"
-              aria-current="page"
-              className="text-gray-900 rounded-l-lg  group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10"
-            >
-              <span>Recent</span>
-              <span
-                aria-hidden="true"
-                className="bg-brand-500 absolute inset-x-0 bottom-0 h-0.5"
-              ></span>
-            </a>
-
-            <a
-              href="/"
-              aria-current="false"
-              className="text-gray-500 hover:text-gray-700   group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10"
-            >
-              <span>Most Liked</span>
-              <span
-                aria-hidden="true"
-                className="bg-transparent absolute inset-x-0 bottom-0 h-0.5"
-              ></span>
-            </a>
-
-            <a
-              href="/"
-              aria-current="false"
-              className="text-gray-500 hover:text-gray-700  rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10"
-            >
-              <span>Most Answers</span>
-              <span
-                aria-hidden="true"
-                className="bg-transparent absolute inset-x-0 bottom-0 h-0.5"
-              ></span>
-            </a>
-          </nav>
-        </div>
-      </div>
       <div className="mt-4">
-        <h1 className="sr-only">Recent questions</h1>
+        <h1 className="sr-only">Recent updates</h1>
         <ul className="space-y-4">
           {updates.map((update) => (
             <HomePageFeedUpdate update={update} key={update.id} />
