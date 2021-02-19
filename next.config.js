3153b15a2a4a2aacb937343162c777091259d112
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-02-19 19:20:56 +0530

    Add temporary redirect from /markdown to wiki page

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
   images: {
     domains: [],
   },
+  async redirects() {
+    return [
+      {
+        source: '/markdown',
+        destination:
+          'https://github.com/coderplex-org/coderplex-org/wiki/Markdown-Guide',
+        permanent: false,
+      },
+    ]
+  },
 }
