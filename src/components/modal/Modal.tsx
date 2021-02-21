2c1ee00ba92285df663a854ead95fc7330a3e9f0
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-02-21 14:37:34 +0530

    Close the modal when clicked on overlay

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -38,7 +38,10 @@ export default function Modal({
           className="fixed inset-0 transition-opacity"
           aria-hidden="true"
         >
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          <div
+            className="absolute inset-0 bg-gray-500 opacity-75"
+            onClick={() => setIsOpen(false)}
+          ></div>
         </Transition>
 
         <span
