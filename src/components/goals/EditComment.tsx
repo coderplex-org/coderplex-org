4e4bc189edc0c750c670e996fcd0b79ded9b1289
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-02-25 13:33:12 +0530

    Add a way to edit comment

---
+++ src/components/goals/EditComment.tsx
@@ -0,0 +1,125 @@
+import { User } from 'src/pages/members'
+import { Avatar, Button, TextArea } from '@/ui'
+import { useForm } from 'react-hook-form'
+import { useSession } from 'next-auth/client'
+import { useMutation, useQueryClient } from 'react-query'
+import toast, { Toaster } from 'react-hot-toast'
+import { useRef, useState } from 'react'
+import { Markdown, A } from '@/components'
+
+type Inputs = {
+  description: string
+}
+
+export default function EditComment({
+  updateId,
+  comment,
+  cancelEditMode,
+}: {
+  updateId: string
+  comment: { id: string; description: string }
+  cancelEditMode: () => void
+}) {
+  const [descriptionStorage, setDescriptionStorage] = useState(
+    comment.description
+  )
+  const queryClient = useQueryClient()
+  const [session, loading] = useSession()
+  const { handleSubmit, register, errors, reset } = useForm<Inputs>()
+  const toastId = useRef('')
+  const { mutate } = useMutation(
+    (data: Inputs) =>
+      fetch(`/api/fauna/goals/edit-comment`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: comment.id,
+          description: data.description,
+        }),
+      }).then((res) => {
+        if (!res.ok) {
+          throw new Error('Something went wrong!!')
+        }
+        return res.json()
+      }),
+    {
+      onSuccess: () => {
+        toast.success('You have successfully updated your comment.', {
+          id: toastId.current,
+        })
+        queryClient.refetchQueries('/api/fauna/all-updates')
+        cancelEditMode()
+        reset()
+      },
+      onError: () => {
+        toast.error('Something went wrong!!!', { id: toastId.current })
+      },
+    }
+  )
+  const onSubmit = (data: Inputs) => {
+    const id = toast.loading('Saving your comment...')
+    toastId.current = id
+    mutate(data)
+    setDescriptionStorage('')
+  }
+  if (loading) {
+    return <p>loading...</p>
+  }
+  return (
+    <>
+      <Toaster />
+      <div className="mt-6">
+        <div className="flex space-x-3">
+          <div className="flex-shrink-0">
+            <div className="relative">
+              <A href={`/${(session.user as User).username}`}>
+                <Avatar
+                  src={(session.user as User).image}
+                  alt={(session.user as User).account?.firstName}
+                />
+              </A>
+            </div>
+          </div>
+          <div className="min-w-0 flex-1">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <TextArea
+                ref={register({ required: true })}
+                label="Comment"
+                defaultValue={comment.description}
+                name="description"
+                hideLabel={true}
+                rows={3}
+                placeholder="Give suggestions or ask questions related to the update."
+                hasError={Boolean(errors.description)}
+                errorMessage="Comment is required!!!"
+                helpText="[Basic Markdown](/markdown) is supported."
+                onKeyDown={(e) => {
+                  if (e.code === 'Enter' && (e.metaKey || e.ctrlKey)) {
+                    handleSubmit(onSubmit)()
+                  }
+                }}
+                onChange={(e) => {
+                  setDescriptionStorage(e.target.value)
+                }}
+              ></TextArea>
+              {descriptionStorage && (
+                <div className="prose max-w-none mt-2 border rounded p-4">
+                  <Markdown>{descriptionStorage}</Markdown>
+                </div>
+              )}
+
+              <div className="mt-6 flex items-center justify-end space-x-4">
+                <Button onClick={() => cancelEditMode()}>Cancel</Button>
+                <Button variant="solid" variantColor="brand" type="submit">
+                  Add Comment
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/components/goals/UpdateComment.tsx
+++ src/components/goals/UpdateComment.tsx
@@ -1,11 +1,17 @@
-import { Avatar } from '@/ui'
+import { Avatar, Menu } from '@/ui'
 import React, { useState } from 'react'
-import { Markdown, A, LikeModal, useLikes } from '@/components'
+import { Markdown, A, LikeModal, useLikes, EditComment } from '@/components'
 import { DateTime } from 'luxon'
 import { UpdateCommentType } from 'src/pages'
 import { useSession } from 'next-auth/client'
 import classNames from 'classnames'
-import { ThumbsUp } from 'phosphor-react'
+import {
+  DotsThreeOutlineVertical,
+  Pencil,
+  ThumbsUp,
+  Trash,
+} from 'phosphor-react'
+import { User } from 'src/pages/members'
 
 export type GoalUpdateType = {
   id: string
@@ -14,14 +20,17 @@ export type GoalUpdateType = {
 }
 
 export default function UpdateComment({
+  updateId,
   comment,
   children,
   isLastComment = false,
 }: {
+  updateId: string
   children: string
   comment: UpdateCommentType
   isLastComment?: boolean
 }) {
+  const [isInEditMode, setIsInEditMode] = useState(false)
   const postedOn = DateTime.fromMillis(comment.createdAt)
   const [session] = useSession()
   const [isLikeModalOpen, setIsLikeModalOpen] = useState(false)
@@ -43,75 +52,120 @@ export default function UpdateComment({
   })
   return (
     <li>
-      <div className="relative pb-6">
-        <div className="relative flex items-start space-x-3">
-          <div className="relative">
-            <A href={`/${comment.postedBy.username}`}>
-              <Avatar
-                src={comment.postedBy.image}
-                alt={comment.postedBy.account?.firstName}
-              />
-            </A>
-          </div>
-          <div className="min-w-0 flex-1">
-            <div>
-              <div className="text-sm">
-                <a
-                  href={`/${comment.postedBy.username}`}
-                  className="font-medium text-gray-900"
-                >
-                  {comment.postedBy.account?.firstName ?? comment.postedBy.name}
-                </a>
-              </div>
-              <p className="mt-0.5 text-sm text-gray-500">
-                Posted on{' '}
-                <time dateTime={postedOn.toISO()}>
-                  {postedOn.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}
-                </time>
-              </p>
+      {isInEditMode ? (
+        <EditComment
+          updateId={updateId}
+          comment={comment}
+          cancelEditMode={() => setIsInEditMode(false)}
+        />
+      ) : (
+        <div className="relative pb-6">
+          <div className="relative flex items-start space-x-3">
+            <div className="relative">
+              <A href={`/${comment.postedBy.username}`}>
+                <Avatar
+                  src={comment.postedBy.image}
+                  alt={comment.postedBy.account?.firstName}
+                />
+              </A>
             </div>
-            <div className="mt-2 text-sm text-gray-700">
-              <div className="prose prose-sm max-w-none">
-                <Markdown>{children}</Markdown>
+            <div className="min-w-0 flex-1">
+              <div className="flex space-x-3">
+                <div className="flex-1">
+                  <div className="text-sm">
+                    <a
+                      href={`/${comment.postedBy.username}`}
+                      className="font-medium text-gray-900"
+                    >
+                      {comment.postedBy.account?.firstName ??
+                        comment.postedBy.name}
+                    </a>
+                  </div>
+                  <p className="mt-0.5 text-sm text-gray-500">
+                    Posted on{' '}
+                    <time dateTime={postedOn.toISO()}>
+                      {postedOn.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}
+                    </time>
+                  </p>
+                </div>
+                {session && (session.user as User).id === comment.postedBy.id && (
+                  <div className="flex-shrink-0 self-center flex">
+                    <div className="relative inline-block text-left">
+                      <Menu
+                        trigger={
+                          <button className="-m-2 p-2 rounded-full flex items-center text-gray-400 hover:text-gray-600">
+                            <span className="sr-only">Open quick actions</span>
+                            <DotsThreeOutlineVertical
+                              className="h-5 w-5"
+                              aria-hidden={true}
+                            />
+                          </button>
+                        }
+                      >
+                        <Menu.Item
+                          icon={Pencil}
+                          onClick={() => setIsInEditMode(true)}
+                        >
+                          Edit
+                        </Menu.Item>
+                        <Menu.Item
+                          icon={Trash}
+                          onClick={() => {
+                            // deleteUpdate()
+                            // const id = toast.loading('Deleting your update...')
+                            // toastId.current = id
+                          }}
+                        >
+                          Delete
+                        </Menu.Item>
+                      </Menu>
+                    </div>
+                  </div>
+                )}
               </div>
-            </div>
+              <div className="mt-2 text-sm text-gray-700">
+                <div className="prose prose-sm max-w-none">
+                  <Markdown>{children}</Markdown>
+                </div>
+              </div>
 
-            <div className="mt-3 flex justify-between space-x-8">
-              <div className="flex space-x-6">
-                <span className="inline-flex items-center text-sm">
-                  <button
-                    className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
-                    onClick={() => {
-                      if (!session) {
-                        setIsLikeModalOpen(true)
-                        return
-                      }
-                      toggleLike()
-                    }}
-                  >
-                    <ThumbsUp
-                      className={classNames(
-                        'h-5 w-5',
-                        hasLiked && 'text-brand-600'
-                      )}
-                      weight={hasLiked ? 'bold' : 'regular'}
+              <div className="mt-3 flex justify-between space-x-8">
+                <div className="flex space-x-6">
+                  <span className="inline-flex items-center text-sm">
+                    <button
+                      className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                      onClick={() => {
+                        if (!session) {
+                          setIsLikeModalOpen(true)
+                          return
+                        }
+                        toggleLike()
+                      }}
+                    >
+                      <ThumbsUp
+                        className={classNames(
+                          'h-5 w-5',
+                          hasLiked && 'text-brand-600'
+                        )}
+                        weight={hasLiked ? 'bold' : 'regular'}
+                      />
+                      <span className="font-medium text-gray-900">
+                        {likesCount}
+                      </span>
+                      <span className="sr-only">likes</span>
+                    </button>
+                    <LikeModal
+                      user={comment.postedBy}
+                      isOpen={isLikeModalOpen}
+                      setIsOpen={setIsLikeModalOpen}
                     />
-                    <span className="font-medium text-gray-900">
-                      {likesCount}
-                    </span>
-                    <span className="sr-only">likes</span>
-                  </button>
-                  <LikeModal
-                    user={comment.postedBy}
-                    isOpen={isLikeModalOpen}
-                    setIsOpen={setIsLikeModalOpen}
-                  />
-                </span>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </li>
   )
 }
