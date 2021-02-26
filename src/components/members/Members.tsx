85eddba340abf7558cbb8651b79087e102468874
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-02-26 12:42:54 +0530

    Show members count in the members page

--- src/components/members/Members.tsx
+++ src/components/members/Members.tsx
@@ -3,7 +3,16 @@ import Member from './Member'
 
 export default function Members({ users }: { users: User[] }) {
   return (
-    <div className="mx-auto max-w-7xl">
+    <div className="mx-auto max-w-7xl space-y-8 sm:space-y-12">
+      <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+        <h2 className="text-black text-3xl font-extrabold tracking-tight sm:text-4xl">
+          Coderplex Community
+        </h2>
+        <p className="text-xl text-gray-500">
+          <span className="text-brand-600 font-bold">{users.length}</span>{' '}
+          people are a part of this amazing community.
+        </p>
+      </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
         {users.map((user) => (
           <Member user={user} key={user.id} />
