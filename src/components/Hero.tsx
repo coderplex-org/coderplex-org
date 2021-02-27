c913beb4397f56d4864e5a025a7fa80845dd7c2b
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-02-28 02:58:03 +0530

    Update title in home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
             Achieve your goals with a community of passionate self-learners by
-            joining the Coderplex Community
+            joining the Coderplex Community.
           </p>
           <div className="mt-5 max-w-xl mx-auto sm:flex sm:justify-center md:mt-8">
             <div className="rounded-md shadow">
