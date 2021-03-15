52dc63f130cdf90be789419c96fbb09e817d558f
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-03-15 13:36:36 +0530

    Refactor

--- src/utils/fauna.ts
+++ src/utils/fauna.ts
@@ -3,6 +3,30 @@ import faunadb from 'faunadb'
 import { User } from 'src/pages/members'
 const q = faunadb.query
 
+function isFollowingUser({
+  session,
+  userRef,
+}: {
+  session: any
+  userRef: Expr
+}) {
+  let isFollowing = false
+
+  if (session) {
+    const followerId = (session.user as User).id
+    const ref = q.Match(q.Index('unique_user_and_follower'), [
+      userRef,
+      q.Ref(q.Collection('users'), followerId),
+    ])
+    isFollowing = q.If(
+      q.Exists(ref),
+      q.Select(['data', 'isFollowing'], q.Get(ref)),
+      false
+    ) as boolean
+  }
+  return isFollowing
+}
+
 export function getUserFromUserRef({
   ref,
   session,
@@ -12,20 +36,8 @@ export function getUserFromUserRef({
 }) {
   const userDoc = q.Get(ref)
   const userId = q.Select(['ref', 'id'], userDoc)
-  let isFollowing = false
+  const isFollowing = isFollowingUser({ session, userRef: ref })
 
-  if (session) {
-    const followerId = (session.user as User).id
-    const ref = q.Match(q.Index('unique_user_and_follower'), [
-      q.Ref(q.Collection('users'), userId),
-      q.Ref(q.Collection('users'), followerId),
-    ])
-    isFollowing = q.If(
-      q.Exists(ref),
-      q.Select(['data', 'isFollowing'], q.Get(ref)),
-      false
-    ) as boolean
-  }
   return {
     id: userId,
     name: q.Select(['data', 'name'], userDoc, null),
@@ -40,6 +52,107 @@ export function getUserFromUserRef({
   }
 }
 
+function hasLikedUpdate({
+  session,
+  goalUpdateRef,
+}: {
+  session: any
+  goalUpdateRef: Expr
+}) {
+  let hasLiked = false
+  if (session) {
+    const userId = (session.user as User).id
+    const ref = q.Match(q.Index('unique_update_user_like'), [
+      goalUpdateRef,
+      q.Ref(q.Collection('users'), userId),
+    ])
+    hasLiked = q.If(
+      q.Exists(ref),
+      q.Select(['data', 'liked'], q.Get(ref)),
+      false
+    ) as boolean
+  }
+  return hasLiked
+}
+
+function hasLikedComment({
+  commentRef,
+  session,
+}: {
+  commentRef: Expr
+  session: any
+}) {
+  let hasLiked = false
+  if (session) {
+    const userId = (session.user as User).id
+    const ref = q.Match(q.Index('unique_comment_user_like'), [
+      commentRef,
+      q.Ref(q.Collection('users'), userId),
+    ])
+    hasLiked = q.If(
+      q.Exists(ref),
+      q.Select(['data', 'liked'], q.Get(ref)),
+      false
+    ) as boolean
+  }
+  return hasLiked
+}
+
+function getAllUsersWhoLikedUpdate({
+  goalUpdateRef,
+  session,
+}: {
+  goalUpdateRef: Expr
+  session: any
+}) {
+  return q.Map(
+    q.Filter(
+      q.Paginate(q.Match(q.Index('all_likes_by_update'), goalUpdateRef)),
+      (updateLikeRef) => q.Select(['data', 'liked'], q.Get(updateLikeRef))
+    ),
+    (likeRef) =>
+      getUserFromUserRef({
+        ref: q.Select(['data', 'user'], q.Get(likeRef)),
+        session,
+      })
+  )
+}
+
+function getAllUsersWhoLikedComment({
+  commentRef,
+  session,
+}: {
+  commentRef: Expr
+  session
+}) {
+  return q.Map(
+    q.Filter(
+      q.Paginate(q.Match(q.Index('all_likes_by_comment'), commentRef)),
+      (commentLikeRef) => {
+        return q.Select(['data', 'liked'], q.Get(commentLikeRef))
+      }
+    ),
+    (likeRef) =>
+      getUserFromUserRef({
+        ref: q.Select(['data', 'user'], q.Get(likeRef)),
+        session,
+      })
+  )
+}
+
+function getAllCommentsOfUpdate({
+  goalUpdateRef,
+  session,
+}: {
+  goalUpdateRef: Expr
+  session: any
+}) {
+  return q.Map(
+    q.Paginate(q.Match(q.Index('all_comments_by_update'), goalUpdateRef)),
+    (commentRef) => getCommentFromCommentRef({ ref: commentRef, session })
+  )
+}
+
 export function getUpdateFromUpdateRef({
   ref: goalUpdateRef,
   session,
@@ -51,66 +164,26 @@ export function getUpdateFromUpdateRef({
   const goalDoc = q.Get(q.Select(['data', 'goal'], goalUpdateDoc))
   const postedByDoc = q.Get(q.Select(['data', 'postedBy'], goalUpdateDoc))
   const description = q.Select(['data', 'description'], goalUpdateDoc)
-
   const createdAt = q.ToMillis(
     q.Select(['data', 'timestamps', 'createdAt'], goalUpdateDoc)
   )
   const updateId = q.Select(['ref', 'id'], goalUpdateDoc)
-  let hasLiked = false
-  if (session) {
-    const userId = (session.user as User).id
-    const ref = q.Match(q.Index('unique_update_user_like'), [
-      q.Ref(q.Collection('goal_updates'), updateId),
-      q.Ref(q.Collection('users'), userId),
-    ])
-    hasLiked = q.If(
-      q.Exists(ref),
-      q.Select(['data', 'liked'], q.Get(ref)),
-      false
-    ) as boolean
-  }
+  const hasLiked = hasLikedUpdate({ goalUpdateRef, session })
   return {
     id: updateId,
     goal: {
       id: q.Select(['ref', 'id'], goalDoc),
       title: q.Select(['data', 'title'], goalDoc),
     },
-    comments: q.Map(
-      q.Paginate(q.Match(q.Index('all_comments_by_update'), goalUpdateRef)),
-      (commentRef) => {
-        return getCommentFromCommentRef({ ref: commentRef, session })
-      }
-    ),
+    comments: getAllCommentsOfUpdate({ goalUpdateRef, session }),
     hasLiked,
-    likes: q.Map(
-      q.Filter(
-        q.Paginate(q.Match(q.Index('all_likes_by_update'), goalUpdateRef)),
-        (updateLikeRef) => {
-          return q.Select(['data', 'liked'], q.Get(updateLikeRef))
-        }
-      ),
-      (likeRef) => {
-        const likeDoc = q.Get(likeRef)
-        const userRef = q.Select(['data', 'user'], likeDoc)
-
-        return getUserFromUserRef({ ref: userRef, session })
-      }
-    ),
+    likes: getAllUsersWhoLikedUpdate({ goalUpdateRef, session }),
     description,
     createdAt,
-    postedBy: {
-      id: q.Select(['ref', 'id'], postedByDoc),
-      name: q.Select(['data', 'name'], postedByDoc, null),
-      image: q.Select(['data', 'image'], postedByDoc, null),
-      username: q.Select(['data', 'username'], postedByDoc, null),
-      account: {
-        firstName: q.Select(
-          ['data', 'account', 'firstName'],
-          postedByDoc,
-          null
-        ),
-      },
-    },
+    postedBy: getUserFromUserRef({
+      ref: q.Select(['ref'], postedByDoc),
+      session,
+    }),
   }
 }
 
@@ -123,53 +196,19 @@ export function getCommentFromCommentRef({
 }) {
   const commentDoc = q.Get(commentRef)
   const postedByDoc = q.Get(q.Select(['data', 'postedBy'], commentDoc))
-  let hasLiked = false
-  const commentId = q.Select(['ref', 'id'], commentDoc)
-  if (session) {
-    const userId = (session.user as User).id
-    const ref = q.Match(q.Index('unique_comment_user_like'), [
-      q.Ref(q.Collection('update_comments'), commentId),
-      q.Ref(q.Collection('users'), userId),
-    ])
-    hasLiked = q.If(
-      q.Exists(ref),
-      q.Select(['data', 'liked'], q.Get(ref)),
-      false
-    ) as boolean
-  }
+  const hasLiked = hasLikedComment({ commentRef, session })
   return {
-    id: commentId,
+    id: q.Select(['ref', 'id'], commentDoc),
     updateId: q.Select(['data', 'update', 'id'], commentDoc),
     description: q.Select(['data', 'description'], commentDoc),
     createdAt: q.ToMillis(
       q.Select(['data', 'timestamps', 'createdAt'], commentDoc)
     ),
     hasLiked,
-    likes: q.Map(
-      q.Filter(
-        q.Paginate(q.Match(q.Index('all_likes_by_comment'), commentRef)),
-        (commentLikeRef) => {
-          return q.Select(['data', 'liked'], q.Get(commentLikeRef))
-        }
-      ),
-      (likeRef) => {
-        const likeDoc = q.Get(likeRef)
-        const userRef = q.Select(['data', 'user'], likeDoc)
-        return getUserFromUserRef({ ref: userRef, session })
-      }
-    ),
-    postedBy: {
-      id: q.Select(['ref', 'id'], postedByDoc),
-      name: q.Select(['data', 'name'], postedByDoc, null),
-      image: q.Select(['data', 'image'], postedByDoc, null),
-      username: q.Select(['data', 'username'], postedByDoc, null),
-      account: {
-        firstName: q.Select(
-          ['data', 'account', 'firstName'],
-          postedByDoc,
-          null
-        ),
-      },
-    },
+    likes: getAllUsersWhoLikedComment({ commentRef, session }),
+    postedBy: getUserFromUserRef({
+      ref: q.Select(['ref'], postedByDoc),
+      session,
+    }),
   }
 }
